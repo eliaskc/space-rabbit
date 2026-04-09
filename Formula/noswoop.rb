962060a7860ffe18a579ac17ba7d9078ca54b5fd
Yaël Guilloux <yael.guilloux@gmail.com>
2026-04-09 23:18:23 +0200

    chore: update formula for v0.4.0

--- Formula/noswoop.rb
+++ Formula/noswoop.rb
@@ -1,7 +1,7 @@
 class Noswoop < Formula
   desc "Disable macOS space-switching animation"
   homepage "https://github.com/tahul/noswoop"
-  url "https://github.com/tahul/noswoop/archive/refs/tags/v0.3.0.tar.gz"
+  url "https://github.com/tahul/noswoop/archive/refs/tags/v0.4.0.tar.gz"
   sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
   license "Unlicense"
 
